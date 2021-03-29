87d15e13c44f9b776fd1ff223aa3723d40013aea
stephanie yoe <stephanie@yoe@gmail.com>
2021-03-29 09:57:27 +0700

    Repo Baru

---
+++ Session 3/mod.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+s = "Hacktiv8-PTP Python for Data Science"
+a = [100, 200, 300]
+
+def foo (arg):
+    print (f'arg = {arg}')
+    
+class Foo:
+    pass
+
